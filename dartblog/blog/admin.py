45a836e4efb39b01651cc62b80db4affcc59cd2c
BeloborodovGIT <73323084+BeloborodovGIT@users.noreply.github.com>
2021-08-20 13:13:33 +0700

    Ckeditor DebugToolbar

--- dartblog/blog/admin.py
+++ dartblog/blog/admin.py
@@ -1,18 +1,30 @@
+from django import forms
 from django.contrib import admin
-
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from .models import *
 
 
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name
+        model = Post
+        fields = '__all__'
+
+
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['title'],}
+    prepopulated_fields = {'slug': ('title',)}
+    form = PostAdminForm
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['title'],}
+    prepopulated_fields = {'slug': ('title',)}
 
 
 class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['title'],}
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Category, CategoryAdmin)
